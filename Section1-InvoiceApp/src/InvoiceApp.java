79ca034659b6c4dc47e58a85086564c96283d65e
RohanOelofse <rohanoelofse1@gmail.com>
2023-01-27 09:49:22 -0600

    Made majority of the assignment changes. Prompt the user for number of invoices

--- Section1-InvoiceApp/src/InvoiceApp.java
+++ Section1-InvoiceApp/src/InvoiceApp.java
@@ -1,8 +1,31 @@
 import java.util.Scanner;
 
+/******************************************
+ * 
+Description: First assignment
+Programmer Rohan Oelofse
+Date written: 2023.01.27
+GitHub: https://github.com/RohanOelofse/JavaProjects
+
+ *****************************************/
+
 public class InvoiceApp {
 
     public static void main(String[] args) {
+    	
+    	// initialize variables for use in calculating averages
+    	int invoiceCount = 0;
+    	int lineItems = 0;
+        double invoiceTotal = 0.0;
+        double discountTotal = 0.0;
+        double discountPercent = 0.0;
+        double subTotal = 0.0;
+        double discountAmount = 0.0;
+        double total = 0.0;
+        
+        String choice = "y";
+        String input = "?";
+        
         // welcome the user to the program
         System.out.println("Welcome to the Invoice Total Calculator");
         System.out.println();  // print a blank line
@@ -10,32 +33,35 @@ public class InvoiceApp {
         // create a Scanner object named sc
         Scanner sc = new Scanner(System.in);
         
-        // initialize variables for use in calculating averages
-        double invoiceTotal = 0.0;
-        double discountTotal = 0.0;
-        int invoiceCount = 0;
+        
 
         // perform invoice calculations until choice is "n" or "N" 
-        String choice = "y";
+       
         while (!choice.equalsIgnoreCase("n")) {
-            // get the invoice subtotal from the user
-            System.out.print("Enter subtotal:   ");
-            String input = sc.nextLine();
-            double subtotal = Double.parseDouble(input);
+        	
+        	// get the invoice line items from the user
+            System.out.print("Enter the number of invoice line items:   ");
+            lineItems = sc.nextInt();
+        	
+        	for(int i = 0; i < lineItems; i++)
+        	{
+        		System.out.print("Enter line item amount:   ");
+                subTotal += sc.nextDouble();
+        	}
+           
 
             // calculate the discount amount and total
-            double discountPercent;
-            if (subtotal >= 500) {
-                discountPercent = .25;
-            } else if (subtotal >= 200) {
-                discountPercent = .2;
-            } else if (subtotal >= 100) {
-                discountPercent = .1;
+            if (subTotal >= 500) {
+                discountPercent = 0.25;
+            } else if (subTotal >= 200) {
+                discountPercent = 0.2;
+            } else if (subTotal >= 100) {
+                discountPercent = 0.1;
             } else {
                 discountPercent = 0.0;
             }
-            double discountAmount = subtotal * discountPercent;
-            double total = subtotal - discountAmount;
+            discountAmount = subTotal * discountPercent;
+            total = subTotal - discountAmount;
             
             // accumulate the invoice count and invoice total
             invoiceTotal = invoiceTotal + total;
